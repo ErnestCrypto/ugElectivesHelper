7a177b2e46db7fa2101adcbb81eaad6396c58aef
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-10-04 16:07:00 +0000

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,3 @@
-var selectedElement = document.querySelectorAll("[id='option']");
-for(var i = 0; i < selectedElement.length; i++) 
-{
-selectedElement[i].addEventListener('click', function(){
-var selected = document.querySelector('.selected')
-if(selected)
-{
-    selected.classList.remove('selected');
-}
-  this.classList.toggle('selected');
-  });
-}
-
-
 var myCenter1 = document.getElementById('myCenter1');
 var myCenter2 = document.getElementById('myCenter2');
 var student = document.getElementById('student');
