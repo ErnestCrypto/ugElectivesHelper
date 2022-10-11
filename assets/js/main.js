09065273100b7532b09dff0c258d009010f4ef3d
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-10-11 16:55:39 +0000

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,20 @@
 
-var myCenter1 = document.getElementById('myCenter1');
-var myCenter2 = document.getElementById('myCenter2');
-var student = document.getElementById('student');
-var staff = document.getElementById('staff');
+let myCenter1 = document.getElementById('myCenter1');
+let myCenter2 = document.getElementById('myCenter2');
+let student = document.getElementById('student');
+let staff = document.getElementById('staff');
+const mainBody = document.querySelectorAll('.main_body');
+const nextBtn = document.querySelectorAll('#nextButton');
+ 
+
+for(let i=0;i<mainBody.length;i++){
+  nextBtn[i].onclick=()=>{mainBody[i].style.cssText="display:none;"}
+  nextBtn[mainBody.length-1].onclick=()=>{mainBody[mainBody.length-1].style.cssText="display:block;"}
+ mainBody[i].style.cssText="z-index:" +(100000-(i*10)) + ";";
+
+
+}
+
 
 student.addEventListener('click', function(){
 if (myCenter1.style.display === "none")
@@ -37,3 +49,13 @@ if (myCenter2.style.display === "none") {
       student.classList.remove("selected");
   }
 });
+
+
+
+
+
+
+
+
+
+
