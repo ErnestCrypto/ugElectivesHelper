8fb0b991fa0db6d68a5bb1d506d40b8e582e7496
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-10-12 15:42:03 +0000

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,17 +5,51 @@ let student = document.getElementById('student');
 let staff = document.getElementById('staff');
 const mainBody = document.querySelectorAll('.main_body');
 const nextBtn = document.querySelectorAll('#nextButton');
- 
+const previousBtn = document.querySelectorAll('#previousButton');
+const elem = document.getElementById("myBar");
+const percent = document.getElementById("myPercent");
+
+elem.addEventListener('click',function () {
+  var i = 0;
+  if (i == 0) {
+    i = 1;
+    var width = 10;
+    
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        percent.innerHTML = width  + "%";
+      }
+    }
+  }
+}
+)
+
 
 for(let i=0;i<mainBody.length;i++){
   nextBtn[i].onclick=()=>{mainBody[i].style.cssText="display:none;"}
-  nextBtn[mainBody.length-1].onclick=()=>{mainBody[mainBody.length-1].style.cssText="display:block;"}
  mainBody[i].style.cssText="z-index:" +(100000-(i*10)) + ";";
 
 
 }
+for(let i=0;i<mainBody.length;i++){
+ 
+  previousBtn[i-2].onclick=()=>{mainBody[i].style.cssText="display:none;"}
+
+}
 
 
+
+
+
+
+
+ 
 student.addEventListener('click', function(){
 if (myCenter1.style.display === "none")
  {
@@ -49,7 +83,6 @@ if (myCenter2.style.display === "none") {
       student.classList.remove("selected");
   }
 });
-
 
 
 
