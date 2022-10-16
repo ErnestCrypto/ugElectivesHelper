6bb3a2fb2e713b7075fa0645b58fcef1960a1b1d
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-10-16 18:37:17 +0000

    commit

--- extra/main.js
+++ extra/main.js
@@ -336,6 +336,23 @@ e.preventDefault();
 //new code
 
 
+//dashboard
+$('.Mymenu-item').each(function(){
+   $(this).click(()=>{
+    $(this).addClass('is-active');
+    console.log('is-active')
+   })
+})
+
+
+
+$('.myMenu-toggle').click(()=>{
+    $('.myMenu-toggle').toggleClass('is-active');
+    $('.sidebar').toggleClass('is-active');
+})
+
+
+
 
 
 
