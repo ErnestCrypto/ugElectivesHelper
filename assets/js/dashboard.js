ff01c2832b576857dc89b01692e0a9ef8d70e197
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-10-16 03:34:36 +0000

    commit

---
+++ assets/js/dashboard.js
@@ -0,0 +1,19 @@
+const menu_toggle = document.querySelector('.myMenu-toggle');
+const sidebar = document.querySelector('.sidebar');
+
+menu_toggle.addEventListener('click',()=>{
+    menu_toggle.classList.toggle('is-active');
+    sidebar.classList.toggle('is-active');
+})
+
+
+
+
+
+
+
+
+
+
+
+
