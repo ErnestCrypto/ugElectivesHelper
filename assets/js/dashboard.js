b1c617ea2b8275f6af2f14c98eb6cbef53ae2b99
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-10-16 04:11:46 +0000

    commit

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,19 +1,17 @@
-const menu_toggle = document.querySelector('.myMenu-toggle');
-const sidebar = document.querySelector('.sidebar');
-
-menu_toggle.addEventListener('click',()=>{
-    menu_toggle.classList.toggle('is-active');
-    sidebar.classList.toggle('is-active');
+$('.Mymenu-item').each(function(){
+   $(this).click(()=>{
+    $(this).addClass('is-active');
+    console.log('is-active')
+   })
 })
 
 
 
+$('.myMenu-toggle').click(()=>{
+    $('.myMenu-toggle').toggleClass('is-active');
+    $('.sidebar').toggleClass('is-active');
 
-
-
-
-
-
+})
 
 
 
